9e5eb3c9231c839ba60fa3dde916aa7ea2d4f36a
nilson <mdnikson30@gmail.com>
2024-09-15 16:03:21 +0100

    update

--- main.tf
+++ main.tf
@@ -1,6 +1,14 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
-resource "aws_s3_bucket" "terraformjuly" {
-  bucket ="terraformjuly"
+resource "aws_s3_bucket" "terraformnik1981" {
+  bucket ="terraformnik1981"
+}
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraformnik1981"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
 }
