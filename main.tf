53c8b91160d597266c00f00330e8a155abc65f53
nilson <mdnikson30@gmail.com>
2024-09-15 07:00:10 -1200

    update

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
-resource "aws_s3_bucket" "terraformjuly" {
-  bucket ="terraformjuly"
+resource "aws_s3_bucket" "terraformren" {
+  bucket ="terraformren"
 }
